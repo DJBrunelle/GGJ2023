28bc01961f234e7b7b6cdd20f8b23ea3b3c5b9a7
Raiford <raiford@thefrozenmachine.com>
2023-02-04 20:18:07 -0700

    Removed the click to level up and added initial stat settings.

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -6,6 +6,7 @@ using Random = UnityEngine.Random;
 public class WaveManager : MonoBehaviour
 {
     [SerializeField] private InputReader _input;
+    [SerializeField] private EventData _eventData;
     
     [SerializeField] private List<Neuron> _neurons;
     [SerializeField] private BadThought _thought;
@@ -15,6 +16,10 @@ public class WaveManager : MonoBehaviour
 
     [SerializeField] private float _eventRate = 10;
     private float _timeSinceEvent;
+
+    [SerializeField] private float _startMaxEnergy = 100f;
+    [SerializeField] private float _startMaxMotivation = 100f;
+    [SerializeField] private float _startMaxTime = 100f;
 
     private EventSimulator _eventSimulator;
 
@@ -45,6 +50,14 @@ public class WaveManager : MonoBehaviour
         _input.ClickEvent += HandleClick;
 
         _eventSimulator = new EventSimulator();
+
+        Stats.maxEnergy = _startMaxEnergy;
+        Stats.maxMotivation = _startMaxMotivation;
+        Stats.maxTime = _startMaxTime;
+        Stats.energy = 0.5f * _startMaxEnergy;
+        Stats.motivation = 0.5f * _startMaxMotivation;
+        Stats.time = 0.5f * _startMaxTime;
+
     }
 
     private void Update()
@@ -57,7 +70,7 @@ public class WaveManager : MonoBehaviour
 
         if (_timeSinceEvent > _eventRate)
         {
-            var gameEvent = _eventSimulator.TriggerEvent();
+            var gameEvent = _eventSimulator.TriggerEvent(_eventData);
             
             // TODO implement text output on UI on random event
         }
@@ -68,7 +81,7 @@ public class WaveManager : MonoBehaviour
 
     private void HandleClick()
     {
-        LevelUp();
-        SpawnThought();
+//        LevelUp();
+//        SpawnThought();
     }
 }
